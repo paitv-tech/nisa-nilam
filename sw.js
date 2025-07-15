c759b47255621dca7d0a62221c5dab245a0e5b49
paitv-tech <pendidikanislam.tv@gmail.com>
2025-07-15 22:37:36 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'nisa-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
